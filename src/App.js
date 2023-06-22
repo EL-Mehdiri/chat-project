cb1472673b95bbefe3ad71e3cee7df4014db34ec
MehdiRidmani <mahdiridmani22@gmail.com>
2023-06-22 13:15:57 +0100

    chat feed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { ChatEngine } from 'react-chat-engine';
 
-import './App.css'
-
+import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 const App = () => {
     return (
         <ChatEngine
             height="100vh"
-            projectID=""
-            userName=""
-            userSecret=""
+            projectID="85f0dff6-1a11-4f8a-ba1b-18bdf03314c1"
+            userName="mehdiridmani"
+            userSecret="qwer1234"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 
         />
     );
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,25 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+
+
+const ChatFeed = (props) => {
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat];
+
+    console.log(chat, userName, messages);
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+    }
+    return (
+        <div>
+            ChatFeed
+
+        </div>
+
+    )
+};
+
+export default ChatFeed;
