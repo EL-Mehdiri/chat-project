abde0573c581a4eaa5aaa579fa5d5d0782446a1b
MehdiRidmani <mahdiridmani22@gmail.com>
2023-06-22 21:50:20 +0100

    adding mymessage and thierms

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,21 @@
 
-const TheirMessage = () => {
+const TheirMessage = ({ message, lastMessage }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
     return (
-        <div>TheirMessage</div>
+        <div className="message-row">
+            {isFirstMessageByUser && (
+                <div className="message-avatar" style={{ backgroundImage: `url(${message?.sender?.avatar})` }} />
+            )}
+            {message?.attachments?.lenght > 0 ?
+                (
+                    <img src={message.attachment[0].file} alt="message-attachment" style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }} />
+                ) :
+                <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+                    {message.text}
+                </div>
+            }
+
+        </div>
     )
 };
 
